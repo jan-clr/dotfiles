13b86cb8f04935d25d7cd4bb98d54abbe1bac1cd
Jan <mail@jan-claar.de>
2022-05-26 13:51:29 +0200

    add thumbnail logic to prep

--- bash/.bashrc
+++ bash/.bashrc
@@ -120,6 +120,13 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+
+# source vte.sh for tilix if exists
+
+#if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+#        source /etc/profile.d/vte.sh
+#fi
+
 ##-----------------------------------------------------
 ## synth-shell-greeter.sh
 #if [ -f /home/jc/.config/synth-shell/synth-shell-greeter.sh ] && [ -n "$( echo $- | grep i )" ]; then
---
+++ pop_os/prepare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+# install fira code font
+sudo apt install fonts-firacode
+
+# gnome related installs
+sudo apt install gnome-tweaks
+sudo apt install libqt5svg5 qtdeclarative5-qtquick2-plugin qtdeclarative5-window-plugin qtdeclarative5-controls-plugin
+
+# thumbnails for mp4 and webp
+sudo apt install ffmpegthumbnailer
+sudo apt install imagemagick
+mkdir -p ~/.local/share/thumbnailers
+echo "[Thumbnailer Entry]
+Exec=/usr/bin/convert %i[0] -thumbnail %sx%s png:%o
+MimeType=image/webp;
+" > ~/.local/share/thumbnailers/webp.thumbnailer
+rm -r ~/.cache/thumbnails/*
