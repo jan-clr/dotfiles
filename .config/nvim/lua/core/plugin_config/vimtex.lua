5b9ce1aab045034dc937ca0731635546c156d485
Jan <mail@jan-claar.de>
2023-04-17 23:07:22 +0200

    feat: change latexmk engine for vimtex

---
+++ .config/nvim/lua/core/plugin_config/vimtex.lua
@@ -0,0 +1,12 @@
+vim.cmd [[
+    let g:vimtex_compiler_latexmk = {
+    \ 'options' : [
+    \   '-lualatex',
+    \   '-shell-escape',
+    \   '-verbose',
+    \   '-file-line-error',
+    \   '-synctex=1',
+    \   '-interaction=nonstopmode',
+    \ ],
+    \} 
+]]
