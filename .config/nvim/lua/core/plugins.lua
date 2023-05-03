78735a7df36b555c2e4192141345cf83a4f05b81
Jan <mail@jan-claar.de>
2023-05-03 23:11:43 +0200

    feat: remove luasnip from NvimTree

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -33,6 +33,7 @@ local plugins = {
       "folke/trouble.nvim",
       dependencies = "nvim-tree/nvim-web-devicons",
   },
+  "folke/zen-mode.nvim",
   {'akinsho/bufferline.nvim', version = "v3.*", dependencies = 'nvim-tree/nvim-web-devicons'},
   'rmagatti/auto-session',
   'Aasim-A/scrollEOF.nvim',
