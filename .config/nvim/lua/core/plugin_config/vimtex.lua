739302c46af07569d9c979ef77b55a9decb5399c
Jan <mail@jan-claar.de>
2023-05-05 15:03:02 +0200

    feat: change latex engine to lualatex

--- .config/nvim/lua/core/plugin_config/vimtex.lua
+++ .config/nvim/lua/core/plugin_config/vimtex.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
+
     let g:vimtex_compiler_latexmk = {
     \ 'options' : [
-    \   '-lualatex',
     \   '-shell-escape',
     \   '-verbose',
     \   '-file-line-error',
@@ -10,5 +10,17 @@ vim.cmd [[
     \ ],
     \} 
 
+    let g:vimtex_compiler_latexmk_engines = {
+        \ '_'                : '-lualatex',
+        \ 'pdfdvi'           : '-pdfdvi',
+        \ 'pdfps'            : '-pdfps',
+        \ 'pdflatex'         : '-pdf',
+        \ 'luatex'           : '-lualatex',
+        \ 'lualatex'         : '-lualatex',
+        \ 'xelatex'          : '-xelatex',
+        \ 'context (pdftex)' : '-pdf -pdflatex=texexec',
+        \ 'context (luatex)' : '-pdf -pdflatex=context',
+        \ 'context (xetex)'  : '-pdf -pdflatex=''texexec --xtx''',
+        \}
     let g:vimtex_view_method = 'zathura'
 ]]
